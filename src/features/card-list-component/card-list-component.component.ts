c9b5cb7ba707f7efa2f590fa25d59446ed927e8a
Helio Fagundes <104528424+Helio-fagundes@users.noreply.github.com>
2025-11-18 20:36:01 +0000

    finalizado, só vou concertar o mailTo e melhorar a validação.

--- src/features/card-list-component/card-list-component.component.ts
+++ src/features/card-list-component/card-list-component.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {CardService} from '../services/card/card.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-card-list-component',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './card-list-component.component.html',
   styleUrl: './card-list-component.component.css'
 })
