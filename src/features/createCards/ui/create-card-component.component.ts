5ca41897f6ee2615c71c6427467b1d189c7c0184
Helio Fagundes <104528424+Helio-fagundes@users.noreply.github.com>
2025-12-18 17:34:30 +0000

    fix: corrigido uns problemas de responsividade

--- src/features/createCards/ui/create-card-component.component.ts
+++ src/features/createCards/ui/create-card-component.component.ts
@@ -7,11 +7,12 @@ import {Router, RouterLink} from '@angular/router';
 import {Cards} from '../../../shared/card/interface/cards';
 import {UiComponentPopup} from '../../../shared/popus/ui/ui.component';
 import {InputFileComponent} from '../../../shared/input-file/input-file.component';
+import { NavbarComponent } from "../../../widgets/layout/navbar/navbar.component";
 
 @Component({
   selector: 'app-create-ui-component',
   standalone: true,
-  imports: [FormsModule, CommonModule, HeaderComponent, RouterLink, UiComponentPopup, InputFileComponent],
+  imports: [FormsModule, CommonModule, HeaderComponent, RouterLink, UiComponentPopup, InputFileComponent, NavbarComponent],
   templateUrl: './create-card-component.component.html',
   styleUrl: './create-card-component.component.css'
 })
